4aade3268d6c8a3072ca89bcede5e50a9bae39d6
de-scientist <gitaumark502@gmail.com>
2025-10-07 12:45:41 +0300

    writing documentation for intellisense

--- src/math.mjs
+++ src/math.mjs
@@ -6,3 +6,9 @@ export function add(a, b) {
 export function sub(a, b) {
     return a - b;
 }
+
+/*+ 
+* @description adds two numbers and returns the result
+* @param a - first number
+* @returns the sum of two numbers
+*/
