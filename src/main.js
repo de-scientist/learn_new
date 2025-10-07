3f4f2a9297522e665d79fb64074ea4bb18eadd86
de-scientist <gitaumark502@gmail.com>
2025-10-07 11:00:00 +0300

    function declaration

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 //arrow function
-const add = (a, b) => console.log(a + b);
+const add = (a, b) =>  console.log(a + b);
+
+const square = number => number ** 2
+
+//function declaration
+function square(number) {
+  return number ** 2;
+}
